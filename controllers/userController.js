7ea93ce7385e8e6552ca08f4287adb7684a2984e
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2023-06-28 19:29:29 +0530

    added error-handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,18 @@
+const expressAsyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 
-const createUser = async(req, res) => {
+const createUser = expressAsyncHandler(async(req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({email:email});
-    console.log(findUser);
     if(!findUser){
         //create new user
         const newUser = await User.create(req.body);
-        console.log(newUser);
         res.json(newUser);
     }else{
         //user already exists
-        res.json({
-            msg:"User already exists",
-            success: false,
-        });
+        throw new Error("user already exists hoe hoe hoe");
+
     };
-};
+});
 
 module.exports = {createUser};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const dbConnect = require('./config/dbConnect');
+const { errorHandler, notFound } = require('./middlewares/errorHandler');
 const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 4000;
@@ -10,8 +11,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 dbConnect();
 
-
 app.use("/api/user", authRouter);
+
+app.use(errorHandler);
+
+app.use(notFound);
+
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
 });
