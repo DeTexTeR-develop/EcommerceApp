53b1fba58df8e08fa8c433fa216a5a0cd8953be2
it's Ujjwal <ujjwalsingh90official@gmail.com>
2023-12-13 16:10:31 +0530

    added basic crud for blogs

--- controllers/blogCtrl.js
+++ controllers/blogCtrl.js
@@ -3,6 +3,25 @@ const validateMongoId = require('../utils/validateMongoDbid');
 const User = require('../models/userModel');
 const expressAsyncHandler = require('express-async-handler');
 
+const getABlog = expressAsyncHandler(async(req,res) => {
+    const {id} = req.params;
+    validateMongoId(id);
+    try{
+        const blog = await Blog.findById(id);
+        res.json(blog);
+    }catch(err){
+        throw new Error("error in finding a Blog", err)
+    }
+});
+
+const getAllBlogs = expressAsyncHandler(async(req, res) => {
+    try{
+        const blogs = await Blog.find();
+        res.json(blogs);
+    }catch(err){
+        throw new Error("error in loading blogs", err);
+    }
+});
 
 const createBlog = expressAsyncHandler(async (req, res) => {
     try {
@@ -22,6 +41,17 @@ validateMongoId(id);
     }catch(err){
         throw new Error("error in updating blog", err);
     }
+});
+
+const deleteBog = expressAsyncHandler(async(req, res)=> {
+    const {id} = req.params;
+    validateMongoId(id);
+    try{
+        const blog = await Blog.findByIdAndDelete(id, req.body);
+        res.json({message:"successfully deleted", blog});
+    }catch(err){
+        throw new Error("error in deleting Blog", err);
+    }
 })
 
-module.exports = {createBlog, updateBlog};
+module.exports = {getABlog, getAllBlogs, createBlog, updateBlog, deleteBog};
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {createBlog , updateBlog} = require('../controllers/blogCtrl');
+const {createBlog , getABlog, getAllBlogs, updateBlog, deleteBog} = require('../controllers/blogCtrl');
 const {authMiddleware} = require('../middlewares/authmiddleware');
 
+router.get('/', authMiddleware, getAllBlogs);
+router.get('/:id', authMiddleware, getABlog);
 router.post('/createBlog',authMiddleware, createBlog);
 router.put('/:id',authMiddleware, updateBlog);
+router.delete('/:id', authMiddleware, deleteBog);
 
 module.exports = router;
